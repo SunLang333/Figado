1f704a19424889930f0f3c4b358e1047cf36662a
TianhaoD <sunlang333@gmail.com>
2025-05-12 20:27:17 +0800

    fix: update ALLOWED_HOSTS in settings.py to allow all hosts for development

--- bookapp_django/bookapp_django/settings.py
+++ bookapp_django/bookapp_django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-=!jjsxvzj(zjnl^8z%w2g!4l@p_$hds(f_r+vw3i03hhz8wb#c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
